ba219089a04f288c943a0b9fed4461158acfc6ca
Marcelo Cesar <marcelo.cesards@gmail.com>
2024-02-11 12:17:39 -0300

    versao inicial de prod

---
+++ src/main/java/br/com/marcelocesar/WorkFlowJavaSpring/WorkFlowJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelocesar.WorkFlowJavaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkFlowJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkFlowJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcelocesar/WorkFlowJavaSpring/WorkFlowJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelocesar.WorkFlowJavaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkFlowJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
